4b6fa3defe14bd6bce44e2c13443eed314ae3197
GabrielUzel <gabriel.uzel1@gmail.com>
2024-02-27 11:01:52 -0300

    Setting MongoDB environment

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
+
 const path = require('path');
 const routes = require('./routes');
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING).then(() => {
+    app.emit("Connected");
+}).catch(e => console.log(e)); // Temporário
+
 const PORT = 5000;
-app.listen(PORT, () => console.log("Server opened..."));
+app.on("Connected", () => {
+    app.listen(PORT, () => console.log("Server opened..."));
+})
+
 
 app.use(routes);
 app.set('views', path.resolve(__dirname, 'src', 'views'));
