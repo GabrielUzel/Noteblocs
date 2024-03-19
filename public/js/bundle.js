42f9614fb1063013521655fc88207b9f1cdf1230
GabrielUzel <gabriel.uzel1@gmail.com>
2024-03-19 17:43:07 -0300

    Add a close panel button to the add notebook form

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6,8 +6,12 @@ var __webpack_exports__ = {};
 var navButton = document.getElementById('nav-button');
 var addNotebookSection = document.getElementById('add-notebook-section');
 var newNotebookForm = document.getElementById('new-notebook-form');
+var closeFormButton = document.getElementById('closeFormButton');
 navButton.addEventListener('click', function () {
   addNotebookSection.style.setProperty('visibility', 'visible');
+});
+closeFormButton.addEventListener('click', function () {
+  addNotebookSection.style.setProperty('visibility', 'hidden');
 });
 /******/ })()
 ;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 const navButton = document.getElementById('nav-button');
 const addNotebookSection = document.getElementById('add-notebook-section');
 const newNotebookForm = document.getElementById('new-notebook-form');
+const closeFormButton = document.getElementById('closeFormButton');
 
 navButton.addEventListener('click', () => {
     addNotebookSection.style.setProperty('visibility', 'visible');
+});
+
+closeFormButton.addEventListener('click', () => {
+    addNotebookSection.style.setProperty('visibility', 'hidden');
 });
